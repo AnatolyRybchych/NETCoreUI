ff9d7fbeae874ea3c36f26bcbd534e51ecfeccaf
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-20 16:00:23 +0300

    X11 lib

--- NETCoreUI/Platform/Linux/LinuxEnvironment.cs
+++ NETCoreUI/Platform/Linux/LinuxEnvironment.cs
@@ -5,17 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using X11;
 
 namespace NETCoreUI.Platform.Linux
 {
     public class LinuxEnvironment : Crossplatform.Environment
     {
+        public IntPtr Display { get; private set; }
+        public int DefaultScreen => X.XDefaultScreen(Display);
+
         private LinuxUIThread linuxUIThread;
         protected override UIThread uiThread => linuxUIThread;
 
         public LinuxEnvironment()
         {
+            Display = X.XOpenDisplay(IntPtr.Zero);
             linuxUIThread = new LinuxUIThread();
+            
         }
 
         protected override IWIndow CreateBaseWindow(string title)
@@ -36,6 +42,11 @@ namespace NETCoreUI.Platform.Linux
         protected override IWIndow CreateBaseWindowWithoutTitleBar(int width, int height)
         {
             throw new NotImplementedException();
+        }
+
+        ~LinuxEnvironment()
+        {
+            X.XCloseDisplay(Display);
         }
     }
 }
--- NETCoreUI/Platform/Linux/LinuxUIThread.cs
+++ NETCoreUI/Platform/Linux/LinuxUIThread.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using X11;
 
 namespace NETCoreUI.Platform.Linux
 {
     public class LinuxUIThread : Crossplatform.UIThread
     {
-        private void HandleMessages()
-        {
-        }
+        private XEvent @event;
 
         protected override void ReceiveMessages()
         {
-            throw new NotImplementedException();
         }
     }
 }
