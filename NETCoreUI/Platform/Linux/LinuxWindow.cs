0e5151a3183a27d60ca1dc698575da738c8acbeb
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-21 16:25:40 +0300

    linux: mouse events

--- NETCoreUI/Platform/Linux/LinuxWindow.cs
+++ NETCoreUI/Platform/Linux/LinuxWindow.cs
@@ -41,14 +41,14 @@ namespace NETCoreUI.Platform.Linux
                     OnMouseMove(new MouseMoveEventArgs(queryPointer.X, queryPointer.Y));
                     break;
                 case EventType.ButtonPress:
-                    Console.WriteLine($"button: {xEvent.xbutton.button}");
+                    Console.WriteLine($"serial: {xEvent.xbutton.serial}");
                     if (xEvent.xbutton.button == 1)
                         OnRightMouseButtonDown(new MouseButtonEventArgs(queryPointer.X, queryPointer.Y));
                     else if (xEvent.xbutton.button == 2)
                         OnLeftMouseButtonDown(new MouseButtonEventArgs(queryPointer.X, queryPointer.Y));
                     break;
                 case EventType.ButtonRelease:
-                    Console.WriteLine($"button: {xEvent.xbutton.button}");
+                    Console.WriteLine($"serial: {xEvent.xbutton.serial}");
                     if (xEvent.xbutton.button == 1)
                         OnRigthMouseButtonUp(new MouseButtonEventArgs(queryPointer.X, queryPointer.Y));
                     else if (xEvent.xbutton.button == 2)
