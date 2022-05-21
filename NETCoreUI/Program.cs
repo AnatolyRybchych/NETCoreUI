6fa7b5e73698735f0f72a1ba0d8480335dc5745d
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-21 16:42:32 +0300

    test events

--- NETCoreUI/Program.cs
+++ NETCoreUI/Program.cs
@@ -15,9 +15,21 @@ namespace NETCoreUI
             {
                 window = ev.CreateWindow("Window");
                 window.RightMouseButtonDown += Window_RightMouseButtonDown;
+                window.LeftMouseButtonDown += Window_LeftMouseButtonDown;
+                window.MouseMove += Window_MouseMove;
             });
 
             ev.JoinUIThread();
+        }
+
+        private static void Window_MouseMove(object sender, IEnvironment environment, Core.WindowEvents.MouseMoveEventArgs e)
+        {
+            Console.WriteLine($"mouseMove {{{e.X}; {e.Y}}}");
+        }
+
+        private static void Window_LeftMouseButtonDown(object sender, IEnvironment environment, Core.WindowEvents.MouseButtonEventArgs e)
+        {
+            Console.WriteLine($"left mouse button {{{e.X}; {e.Y}}}");
         }
 
         private static void Window_RightMouseButtonDown(object sender, IEnvironment environment, Core.WindowEvents.MouseButtonEventArgs e)
