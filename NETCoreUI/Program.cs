554fd925d0e3e6036c8d1d466d513f3c1f60ad33
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-22 20:47:01 +0300

    keyboard events for windows and linux

--- NETCoreUI/Program.cs
+++ NETCoreUI/Program.cs
@@ -14,6 +14,7 @@ namespace NETCoreUI
             ev.UIThread.Execute(() =>
             {
                 window = ev.CreateWindow("Window");
+
                 window.RightMouseButtonDown += Window_RightMouseButtonDown;
                 window.RightMouseButtonUp += Window_RightMouseButtonUp;
 
