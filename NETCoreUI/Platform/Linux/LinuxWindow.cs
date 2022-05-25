86f2da4b5b9ca72c7c96ce7f8f4c9f2342afb41d
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-26 01:58:58 +0300

    test

--- NETCoreUI/Platform/Linux/LinuxWindow.cs
+++ NETCoreUI/Platform/Linux/LinuxWindow.cs
@@ -90,6 +90,7 @@ namespace NETCoreUI.Platform.Linux
                     OnRedraw(new RedrawEventArgs(Graphics));
                     break;
                 case EventType.ConfigureNotify:
+                    Console.WriteLine($"window: {xEvent.xconfigure.window}");
                     OnMove(new MoveEventArgs(new Point(xEvent.xconfigure.x, xEvent.xconfigure.y)));
                     OnResize(new ResizeEventArgs(new Size(xEvent.xconfigure.width, xEvent.xconfigure.height)));
                     break;
