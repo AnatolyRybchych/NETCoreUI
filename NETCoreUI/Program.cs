a2e2ca2159c229e6525726ff80bd425a82348393
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-25 22:47:57 +0300

    test linux graphics

--- NETCoreUI/Program.cs
+++ NETCoreUI/Program.cs
@@ -7,10 +7,10 @@ namespace NETCoreUI
 {
     class Program
     {
-        [DllImport("opengl32.dll")]
+        [DllImport("libGL.dll")]
         public static extern void glClear(int mask);
 
-        [DllImport("opengl32.dll")]
+        [DllImport("libGL.dll")]
         public static extern void glClearColor(float r, float g, float b, float a);
 
         static void Main(string[] args)
