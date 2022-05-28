3ef1b1b2322a3b54a661dc4ad1da5095d40050d1
AnatolyRybchych <tol.ryb@gmail.com>
2022-05-29 02:03:50 +0300

    separate SymbolInterpreter into different file

--- NETCoreUI/EnvironmentProvider.cs
+++ NETCoreUI/EnvironmentProvider.cs
@@ -42,19 +42,4 @@ namespace NETCoreUI
                 return env;
         }
     }
-
-    public static class SymbolInterpreter
-    {
-        public static LinuxEnvironment ToLinux(this IEnvironment env) => (LinuxEnvironment)env;
-        public static NTEnvironment ToWindows(this IEnvironment env) => (NTEnvironment)env;
-
-        public static LinuxWindow ToLinux(this IWIndow wnd) => (LinuxWindow)wnd;
-        public static NTWindow ToWindows(this IWIndow wnd) => (NTWindow)wnd;
-
-        public static LinuxUIThread ToLinux(this IUIThread uith) => (LinuxUIThread)uith;
-        public static NTUIThread ToWindows(this IUIThread uith) => (NTUIThread)uith;
-
-        public static LinuxGraphicsContext ToLinux(this GraphicsContext g) => (LinuxGraphicsContext)g;
-        public static WindowsGraphicsContext ToWindows(this GraphicsContext g) => (WindowsGraphicsContext)g;
-    }
 }
---
+++ NETCoreUI/SymbolInterpreter.cs
@@ -0,0 +1,26 @@
+﻿using NETCoreUI.Core;
+using NETCoreUI.Platform.Linux;
+using NETCoreUI.Platform.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETCoreUI
+{
+    public static class SymbolInterpreter
+    {
+        public static LinuxEnvironment ToLinux(this IEnvironment env) => (LinuxEnvironment)env;
+        public static NTEnvironment ToWindows(this IEnvironment env) => (NTEnvironment)env;
+
+        public static LinuxWindow ToLinux(this IWIndow wnd) => (LinuxWindow)wnd;
+        public static NTWindow ToWindows(this IWIndow wnd) => (NTWindow)wnd;
+
+        public static LinuxUIThread ToLinux(this IUIThread uith) => (LinuxUIThread)uith;
+        public static NTUIThread ToWindows(this IUIThread uith) => (NTUIThread)uith;
+
+        public static LinuxGraphicsContext ToLinux(this GraphicsContext g) => (LinuxGraphicsContext)g;
+        public static WindowsGraphicsContext ToWindows(this GraphicsContext g) => (WindowsGraphicsContext)g;
+    }
+}
