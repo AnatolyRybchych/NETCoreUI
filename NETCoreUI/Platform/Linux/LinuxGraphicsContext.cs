d75a8b37dd4b82d2bd96074ed27250bb6b770659
AnatolyRybchych <tol.ryb@gmail.com>
2022-06-01 22:08:34 +0300

    linux: drawing image with alpha

--- NETCoreUI/Platform/Linux/LinuxGraphicsContext.cs
+++ NETCoreUI/Platform/Linux/LinuxGraphicsContext.cs
@@ -37,8 +37,9 @@ namespace NETCoreUI.Platform.Linux
         public override void DrawImageApplyAlpha(IGraphicsImage image, Size size, Point pos)
         {
             IntPtr img = X.XGetImage(Display, ((LinuxGraphicsImage)image).Pixmap, pos.X, pos.Y, size.Width, size.Height, 1, LinuxImage.ZPixmap);
+            if (img == IntPtr.Zero) throw new Exception("Cannot get XImage from image");
             X.XPutImage(Display, Drawable, Gc, img, 0, 0, pos.X, pos.Y, size.Width, size.Height);
-            X.XDestroyImage(img);
+            //X.XDestroyImage(img);
         }
 
         public LinuxSimpleRenderer LinuxRenderer => (LinuxSimpleRenderer)SimpleRenderer;
